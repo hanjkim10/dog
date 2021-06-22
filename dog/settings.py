5c7fce0b59f792f4610c79a0ae37ef0fb00ec788
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-06-22 15:34:23 +0900

    Add: owner application

--- dog/settings.py
+++ dog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from my_settings import DATABASES, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ze3&!)#)pj8=t95d7cbv9e4p_m3nt)i+$tyv-uxh45rjgmzr-j'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
+    'owners',
 ]
 
 MIDDLEWARE = [
@@ -75,12 +77,7 @@ WSGI_APPLICATION = 'dog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
 
-# Create your views here.
+import json
+
+from django.http import JsonResponse
+from django.views import View
+from owners.models import Owner, Dog 
+
+class OwnerView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        print(data)
+        print(data["name"]) 
+
+        owners = Owner.objects.create(
+            name=data['name'], 
+            email=data['email'], 
+            age=data['age']
+        )
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status = 201)
+
+class DogView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        # print(type(data['name']))
+        dog = Dog.objects.create(
+            name=data['name'],
+            age=data['age'],
+            owner_id=Owner.objects.get(name=data['owner_name'])
+        )
+        return JsonResponse({'MESSAGE':'SUCCESS'}, status = 201)
