4172bc4ae50ab5def9ccf29b838fec2c6dd01d9d
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-06-22 19:25:19 +0900

    Add: dog application

--- owners/views.py
+++ owners/views.py
@@ -17,7 +17,31 @@ class OwnerView(View):
             age=data['age']
         )
         return JsonResponse({'MESSAGE':'SUCCESS'}, status = 201)
+        
+    def get(self, rquest):
+    
+        owners = Owner.objects.all()
+        result_owner = []
+       
+        for owner in owners:
+            dog_list = []
+            dogs = Dog.objects.all()
 
+            for dog in dogs:
+                my_dict = {
+                    'name' : dog.name,
+                    'age': dog.age
+                }
+                dog_list.append(my_dict)
+            owner_dict = {
+            'name' : owner.name,
+            'age' : owner.age,
+            'email' : owner.email,
+            'dog_list' : dog_list
+            }
+            result_owner.append(owner_dict)
+        return JsonResponse({"result": result_owner}, status = 200)
+    
 class DogView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -28,3 +52,18 @@ class DogView(View):
             owner_id=Owner.objects.get(name=data['owner_name'])
         )
         return JsonResponse({'MESSAGE':'SUCCESS'}, status = 201)
+    
+    def get(self, request):
+        dog_list = []
+        dogs = Dog.objects.all()
+        for dog in dogs:
+            my_dict = {
+                'owner' : dog.owner_id.name,
+                'name' : dog.name,
+                'age' : dog.age
+            }
+            dog_list.append(my_dict)
+        return JsonResponse({"result" : dog_list}, status = 200)
+
+
+
